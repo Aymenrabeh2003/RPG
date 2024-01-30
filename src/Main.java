0e15ce757ba73c242178b625c961e5a3c05791d3
Aymenrabeh2003 <aymenforever2003@gmail.com>
2024-01-30 20:46:27 +0100

    nuevas clases

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Character.Race.Humano;
+import Character.Race.Orc;
+import Character.Stat.Dexterity;
+
+public class Main {
+    public static void main(String[] args) {
+        Orc orco1 = new Orc();
+        Humano humano1 = new Humano();
+        orco1.modifier();
+    }
+}
